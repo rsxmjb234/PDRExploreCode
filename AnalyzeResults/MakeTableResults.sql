c0c75db93b5b446c4375f40b60df2578c3a0fa5c
mberger <mberger@LAPTOP-91623757>
2026-07-24 12:54:30 -0400

    routine

--- AnalyzeResults/MakeTableResults.sql
+++ AnalyzeResults/MakeTableResults.sql
@@ -1,21 +1,21 @@
+DROP TABLE IF EXISTS pdr_inventory.ehr_software_analysis;
+
 CREATE EXTERNAL TABLE IF NOT EXISTS pdr_inventory.ehr_software_analysis (
-    path                       STRING,
-    filename                   STRING,
-    qe                         STRING,
-    input_assigning_authority  STRING,
-    processing_time_ms         INT,
-    file_size_bytes            INT,
-    assigning_authority        STRING,
-    oid                        STRING,
-    software_name              STRING,
-    manufacturer_model_name    STRING,
-    custodian_org_name         STRING,
-    has_epic_oid               STRING,
-    epic_oids_found            STRING,
-    ehr_guess                  STRING,
-    ehr_guess_confidence       STRING,
-    ehr_guess_reason           STRING,
-    parse_type                 STRING
+    path                              STRING,
+    filename                          STRING,
+    qe                                STRING,
+    input_assigning_authority         STRING,
+    processing_time_ms                INT,
+    file_size_bytes                   INT,
+    assigning_authority_parsed_from_s3 STRING,
+    oid                               STRING,
+    software_name                     STRING,
+    manufacturer_model_name           STRING,
+    custodian_org_name                STRING,
+    ehr_guess                         STRING,
+    ehr_guess_confidence              STRING,
+    ehr_guess_reason                  STRING,
+    parse_type                        STRING
 )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
